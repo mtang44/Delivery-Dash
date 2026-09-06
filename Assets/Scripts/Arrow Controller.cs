731a1ee6e9d01897c5d7e6a5effccde96aa1a429
zturtledog <confusedparrotfish@gmail.com>
2026-09-06 02:31:16 -0700

    fixed arrow bug, increased arrow time, reorganized(some) main scene

--- Assets/Scripts/Arrow Controller.cs
+++ Assets/Scripts/Arrow Controller.cs
@@ -1,32 +1,27 @@
 using UnityEngine;
 using TMPro;
-public class ArrowController : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+public class ArrowController : MonoBehaviour {
     public float degreeTurnRate = 5f;
-    [SerializeField] Transform destination;
-    [SerializeField] GameObject distanceTMP; 
+    public GameObject distanceTMP; 
+    public arrow_manager manager;
+    public ArrowControl control;
 
-    // Update is called once per frame
-    void Start()
-    {
-       
-    }
-    void Update()
-    {
-        
+    void Update() {
+        arrow_manager.OrderData data = manager.get_order(control.arrow_id);
+        Vector3 destination;
+        if (data.has_been_picked_up) destination = data.delivery.action_location.position;
+        else destination = data.pickup.action_location.position;
+
         float step =  degreeTurnRate * Time.deltaTime;
-        Vector3 targetDirection = destination.position - transform.position;
+        Vector3 targetDirection = destination - transform.position;
         Vector3 turnDirection =  Vector3.RotateTowards(transform.forward, targetDirection, step, 0.0f);
         transform.rotation = Quaternion.LookRotation(turnDirection);
         // distanceTMP.GetComponent<TextMeshProUGUI>().text = "" + findDistanceToTarget() + "M";
-       
     }
 
-    public int findDistanceToTarget()
-    {
-        float distance = Vector3.Distance(transform.position,destination.position);
+    // public int findDistanceToTarget() {
+    //     float distance = Vector3.Distance(transform.position,destination.position);
         
-        return (int)distance;
-    }
+    //     return (int)distance;
+    // }
 }
